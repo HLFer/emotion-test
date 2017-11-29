414e83ba1fd9e3275faa7468421ef735010a3951
Henrique Fernandes <henriquedin@hotmail.com>
2017-11-29 01:05:04 -0200

    Criação das funções de inserção de telefone e busca de CPF.

--- Database.php
+++ Database.php
@@ -19,6 +19,7 @@ Class Database
 
 	public function insertAluno($data)
 	{
+		//Variavel recebe query para inserção na tabela aluno do BD
 		$sql = "INSERT INTO aluno (id, cpf, nome, sexo, dt_nascimento) VALUES (null, '{$data['cpf']}', '{$data['nome']}', '{$data['sexo']}', DATE('{$data['data_nascimento']}'))";
 	
 		try 
@@ -34,18 +35,56 @@ Class Database
 			echo($ex->getMessage()); 
 			exit();
 		}
-	}
 
-	public function buscaCPF($cpf)
+	}
+	//Função para inserir os telefones na tabela telefone do BD
+	public function insertTelefone($id_aluno, $numero, $flag_principal = false)
 	{
-		$sql = "SELECT id FROM aluno WHERE cpf = '{$cpf}'";
+		//Variável recebe query para inserção na tabela telefone do BD
+		$sql = "INSERT INTO telefone (id, id_aluno, numero, flag_principal) VALUES (null, '{$id_aluno}', '{$numero}', '{$flag_principal}')";
 	
 		try 
-		{}
+		{
+			$conn = $this->_connect();
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+
+			$conn = null;
+		}
 		catch (Exception $ex)
 		{
 			echo($ex->getMessage()); 
 			exit();
 		}
+
+	}
+
+	//Função para validar se os CPFs do arquivo telefones.csv são os mesmos da tabela aluno no BD 
+	public function buscaCPF($cpf)
+	{
+
+			//Variável recebe query para selecionar o id correspondente ao CPF pesquisado
+			$sql = "SELECT id FROM aluno WHERE cpf = '{$cpf}'";
+
+			try
+			{
+				$conn = $this->_connect();
+				$stmt = $conn->prepare($sql);
+				$stmt->execute();
+				$conn = null;
+				//Variável que recebe o id selecionado no BD para retorno da função
+				$aluno = $stmt->fetchAll();
+				
+				if(isset($aluno[0]['id']))
+				{
+					return (int)$aluno[0]['id'];
+				}
+				return false;
+			}
+			catch (Exception $ex)
+			{
+				echo($ex->getMessage()); 
+				exit();
+			}
 	}
 }
