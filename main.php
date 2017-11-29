5b991de0a468f4f89c6c198c391891458a9193e0
Henrique Fernandes <henriquedin@hotmail.com>
2017-11-29 01:08:25 -0200

    Criação da função para importação de telefones e função buscaCPF.

--- main.php
+++ main.php
@@ -2,51 +2,75 @@
 
 include "Database.php";
 
-
-
-//variável para receber o nome do arquivo .csv na raiz do projeto
+//variáveis para receber os arquivos .csv localizados na raiz do projeto
 $arquivo_alunos = "alunos.csv";
 $arquivo_telefones = "telefones.csv";
 
+//Chamada das funções para importar alunos e telefones passando os arquivos como parametro
 importarAlunos($arquivo_alunos);
+importarTelefones($arquivo_telefones);
 
-//imprtarTelefones($arquivo_telefones);
-
-
-
-
+//Definição da função de importação de alunos
 function importarAlunos($arquivo)
 {
+	//Criação do Objeto Database
 	$db = new Database();
+
+	//Mapeamento do arquivo .csv e conversão para string - armazenado na varável $alunos
 	$alunos = array_map('str_getcsv', file($arquivo));
 
+	//Loop para coletar os dados dos alunos na planilha
 	foreach ($alunos as $key => $aluno) {
 		
+		//Remoção da primeira linha da planilha
 		if ($key == 0)
 			continue;
-		
+		//Conversão do formato da data coletada
 		$data_nascimento = DateTime::createFromFormat('d/m/y', $aluno[3]);
-	
+		
+		//Variável que recebe os registros e armazena no vetor
 		$data = array(
 				'cpf' => $aluno[0],
 				'nome' => $aluno[1],
-				'sexo' => $aluno[2],
+				'sexo' => $aluno[2],//Data no formato do BD
 				'data_nascimento' => $data_nascimento->format('Y-m-d'),
 				
 	 		);
 
+		//Chamada da função (implementada em Database.php) para inserir os alunos no BD
 		$db->insertAluno($data);
-
 	}
 	
 }
 
-function imprtarTelefones($arquivo)
+//Definição da função de importação de telefones
+function importarTelefones($arquivo)
 {
+	//Criação do Objeto Database
 	$db = new Database();
 
-	//verificar se exsite cpf em aluno
+	//Mapeamento do arquivo .csv e conversão para string - armazenado na varável $telefones
+	$telefones = array_map('str_getcsv', file($arquivo));
 
-	//inseir telefone
-	
+	//Loop para coletar os CPFs e Telefones na planilha
+	foreach ($telefones as $key => $telefone) {
+		
+		//Remoção da primeira linha da planilha
+		if ($key == 0)
+			continue;
+		
+		//Variável que recebe os registros e armazena no vetor
+		$data = array(
+				'cpf' => $telefone[0],
+				'telefone' => $telefone[1]
+	 		);
+
+		//Chamada da função (implementada em Database.php) 
+		//para comparar os CPFs no BD com os CPFs do arquivo
+		$id_aluno = $db->buscaCPF($data['cpf']);
+
+		if($id_aluno){
+			$db->insertTelefone($id_aluno, $data['telefone']);
+		}
+	}
 }
